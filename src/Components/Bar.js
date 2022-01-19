bb00ccbfae4011ac0c227d3214c395a710e80cab
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-01-19 23:12:23 +0530

    adding click to copy

--- src/Components/Bar.js
+++ src/Components/Bar.js
@@ -2,25 +2,56 @@ import React from "react";
 import "./Bar.css";
 
 const Bar = (props) => {
+  const copy1 = () => {
+    navigator.clipboard.writeText(props.color1);
+
+    console.log("c");
+    alert("Hex Code Copied!");
+  };
+  const copy2 = () => {
+    navigator.clipboard.writeText(props.color2);
+    console.log("c");
+
+    alert("Hex Code Copied!");
+  };
+  const copy3 = () => {
+    navigator.clipboard.writeText(props.color3);
+    console.log("c");
+
+    alert("Hex Code Copied!");
+  };
+  const copy4 = () => {
+    navigator.clipboard.writeText(props.color4);
+    console.log("c");
+
+    alert("Hex Code Copied!");
+  };
+  const copy5 = () => {
+    navigator.clipboard.writeText(props.color5);
+    console.log("c");
+
+    alert("Hex Code Copied!");
+  };
   return (
     <div className="main__bar">
       <div
         className="main-bar__inner-bar"
-        id="sample"
         style={{ backgroundColor: `${props.color1}` }}
+        onClick={copy1}
       >
         <span>{props.color1}</span>
       </div>
       <div
         className="main-bar__inner-bar"
         style={{ backgroundColor: `${props.color2}` }}
+        onClick={copy2}
       >
         <span>{props.color2}</span>
       </div>
-
       <div
         className="main-bar__inner-bar"
         style={{ backgroundColor: `${props.color3}` }}
+        onClick={copy3}
       >
         <span>{props.color3}</span>
       </div>
@@ -28,6 +59,7 @@ const Bar = (props) => {
       <div
         className="main-bar__inner-bar"
         style={{ backgroundColor: `${props.color4}` }}
+        onClick={copy4}
       >
         <span>{props.color4}</span>
       </div>
@@ -35,6 +67,7 @@ const Bar = (props) => {
       <div
         className="main-bar__inner-bar"
         style={{ backgroundColor: `${props.color5}` }}
+        onClick={copy5}
       >
         <span>{props.color5}</span>
       </div>
